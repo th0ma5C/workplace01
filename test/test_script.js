479d6968692aaa87baf44d797e7fcb0a24d9fb47
th0ma5C <thomas29111@gmail.com>
2023-10-24 17:18:28 +0800

    1023練習

---
+++ test/test_script.js
@@ -0,0 +1,16 @@
+/*
+------CDN------
+React:
+
+<script crossorigin src="https://unpkg.com/react@18/umd/react.production.min.js"></script>
+<script crossorigin src="https://unpkg.com/react-dom@18/umd/react-dom.production.min.js"></script>
+
+bable:
+
+<script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
+
+jQuery:
+
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+
+*/
