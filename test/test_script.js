902a2cb885cd4d2732a82497cd7b9e9c90381cd6
th0ma5C <thomas29111@gmail.com>
2023-10-30 19:17:38 +0800

    1030課程練習

--- test/test_script.js
+++ test/test_script.js
@@ -13,6 +13,12 @@ jQuery:
 
 <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 
+Bootstrap:
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
 */
 function foo() {
     return 42;
@@ -20,4 +26,4 @@ function foo() {
 
 foo.bar = "Hello";
 
-console.log('foo:',typeof foo());
+console.log('foo:', typeof foo());
