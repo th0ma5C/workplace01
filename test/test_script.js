4d5b255fb517a3ef9f77025c61cf2c183e3133de
th0ma5C <thomas29111@gmail.com>
2023-11-01 16:49:05 +0800

    1101scss,Vue練習

--- test/test_script.js
+++ test/test_script.js
@@ -19,6 +19,10 @@ Bootstrap:
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
+
+Vue:
+
+<script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 */
 function foo() {
     return 42;
