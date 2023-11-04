b50e574d42ea2d5b3a04a9b9c1e98d056c605e44
th0ma5C <thomas29111@gmail.com>
2023-11-04 20:53:40 +0800

    1104React練習

--- test/test_script.js
+++ test/test_script.js
@@ -4,6 +4,8 @@ React:
 
 <script crossorigin src="https://unpkg.com/react@18/umd/react.production.min.js"></script>
 <script crossorigin src="https://unpkg.com/react-dom@18/umd/react-dom.production.min.js"></script>
+PropTypes:
+<script src="https://cdnjs.cloudflare.com/ajax/libs/prop-types/15.6.0/prop-types.min.js"></script>
 
 bable:
 
