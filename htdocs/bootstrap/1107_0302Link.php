c3754e032b846f0f325f008e42fc119cfb6f8d24
th0ma5C <thomas29111@gmail.com>
2023-11-07 16:46:41 +0800

    1107php練習

---
+++ htdocs/bootstrap/1107_0302Link.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <script src="./script/bootstrap.bundle.min.js"></script>
+    <style>
+    .carousel-item img {
+        width: 100vw;
+        height: 30vh;
+        object-fit: cover;
+        object-position: 0 40%;
+    }
+
+    .carousel-caption h5,
+    .carousel-caption p {
+        display: inline-block;
+        background-color: rgba(0, 0, 0, 0.3);
+        border-radius: 20px;
+        box-sizing: border-box;
+        padding: 5px;
+    }
+
+    .container-fluid {
+        padding: 0;
+    }
+    </style>
+</head>
+
+<body>
+    <!-- nav bar -->
+    <?php
+    require('./1107_03navbar.php');
+    ?>
+    <!-- nav bar  end-->
+
+<h1>Welcome!It is link</h1>
+
+</body>
+
+</html>
