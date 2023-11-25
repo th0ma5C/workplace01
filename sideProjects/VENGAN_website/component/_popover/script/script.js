2182294349f0f855c7e6be852c2cae1b42627d73
th0ma5C <thomas29111@gmail.com>
2023-11-25 22:51:04 +0800

    1125popover完善

--- sideProjects/VENGAN_website/component/_popover/script/script.js
+++ sideProjects/VENGAN_website/component/_popover/script/script.js
@@ -2,13 +2,22 @@ const vm = Vue.createApp({
     data() {
         return {
             display: false,
+            tab: 1,
+            header: {
+                title: 1,
+                text: 0,
+                height: 120
+            },
             crd_opn: `<span><img src="./img/x_icon.svg" alt="X" style="padding: 6px 6px 0 6px; width: 36px;"></span>`,
             crd_cls: `<span><img src="./img/Logo1.PNG" alt="LOGO"></span>
                       <span>會員中心</span>`,
+            mber_tab: `<p>請先登入</p>`,
+
         }
     },
     mounted() {
-        document.addEventListener("click", this.bodyClosePop)
+        document.addEventListener("click", this.bodyClosePop);
+
     },
     beforeUnmount() {
         document.removeEventListener("click", this.bodyClosePop)
@@ -22,6 +31,17 @@ const vm = Vue.createApp({
                 this.display = false;
             }
             // this.display = !this.display;
+        },
+        header_collapse(e) {
+            if (e.deltaY > 0) {
+                this.header.title = 0;
+                this.header.text = 1;
+                this.header.height = 53
+            } else {
+                this.header.title = 1;
+                this.header.text = 0;
+                this.header.height = 120
+            }
         },
     }
 
