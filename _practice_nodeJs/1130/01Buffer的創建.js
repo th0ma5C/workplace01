736105c7830b28c7ab2a353b8d98b14b053ce684
th0ma5C <thomas29111@gmail.com>
2023-12-01 17:46:04 +0800

    1201vue練習

---
+++ _practice_nodeJs/1130/01Buffer的創建.js
@@ -0,0 +1,12 @@
+//創建方式有三種
+//1. alloc 會將每個內存數據歸零
+let buf = Buffer.alloc(10)
+// console.log(buf);
+
+//2. allocUnsafe 不會歸零內存數據，但創建速度較快
+let buf2 = Buffer.allocUnsafe(10000);
+// console.log(buf2);
+
+//3. from 建立包含指定字串，數組或緩衝區的新緩衝區。
+let buf3 = Buffer.from("hello")
+console.log(buf3);
---
+++ _practice_nodeJs/1130/02Buffer的操作.js
@@ -0,0 +1,18 @@
+let buf = Buffer.from([105, 108, 111, 118, 101, 121, 111, 117]);
+console.log(buf.toString()); //utf-8 => iloveyou
+
+//[]元素的操作
+let buf2 = Buffer.from("hello");
+console.log(buf2[1].toString(2)); //加上toString(2)可由10進制轉為2進制 101=>01100101
+console.log(buf2);
+buf2[0] = 95;
+console.log(buf2.toString());
+
+//暫存溢出
+console.log(buf2);
+buf2[0] = 361; // 捨棄高位的數字 0001 0110 1001 => 0110 1001
+console.log(buf2);
+
+//中文
+let buf3 = Buffer.from("你好");
+console.log(buf);
