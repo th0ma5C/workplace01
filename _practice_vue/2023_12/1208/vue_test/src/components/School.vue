5583bb06c30b4088566879816abc3727d6524315
th0ma5C <thomas29111@gmail.com>
2023-12-08 22:14:39 +0800

    1208cli練習

---
+++ _practice_vue/2023_12/1208/vue_test/src/components/School.vue
@@ -0,0 +1,34 @@
+<template>
+    <!-- 組件結構 -->
+    <div class="demo">
+        <h2>學校名稱: {{ schoolName }}</h2>
+        <h2>學校地址: {{ schoolAddr }}</h2>
+        <hr>
+        <button @click="showName">提示學校名稱</button>
+    </div>
+</template>
+
+<script>
+// 組件腳本
+export default {
+    name: 'School',
+    data() {
+        return {
+            schoolName: 'Google',
+            schoolAddr: 'Taipei'
+        }
+    },
+    methods: {
+        showName() {
+            alert(this.schoolName)
+        }
+    },
+}
+</script>
+
+<style>
+/* 組件樣式 */
+.demo {
+    background-color: orange;
+}
+</style>
---
+++ _practice_vue/2023_12/1208/vue_test/src/main.js
@@ -0,0 +1,16 @@
+import { createApp, h } from 'vue'
+import App from './App.vue'
+
+// createApp(App).mount('#demo')
+const vm = createApp({
+    // render() {
+    //     return h(App)
+    // },
+    // render() {
+    //     return h(App)
+    // }
+    render: () => h(App)
+    // template: `<app></app>`,
+    // components: { App }
+});
+vm.mount("#demo");
