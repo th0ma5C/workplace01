03dd96a64654b4242fd01c7bba86694a0046aedd
th0ma5C <thomas29111@gmail.com>
2023-12-11 15:06:56 +0800

    1211vue練習

--- _practice_vue/cli_practice/1210_refs/App.vue
+++ _practice_vue/cli_practice/1210_refs/App.vue
@@ -1,14 +1,15 @@
 <template>
     <div>
         <h1 v-text="msg" ref="title"></h1>
+        <button @click="showDOM">點我提示DOM元素</button>
         <School ref="sch" />
     </div>
 </template>
 <script>
-import School from './components/Student.vue';
+import School from './components/School.vue';
 export default {
     name: 'App',
-    components: { Student },
+    components: { School },
     data() {
         return {
             msg: '歡迎光臨'
---
+++ _practice_vue/cli_practice/src/components/Student.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="demo">
+        <h1>{{ msg }}</h1>
+        <h2>學生名稱: {{ name }}</h2>
+        <h2>學生年齡: {{ age }}</h2>
+        <h2>學生性別: {{ sex }}</h2>
+    </div>
+</template>
+<script>
+export default {
+    name: 'my-Student',
+    data() {
+        return {
+            msg: '我是一位學生',
+            name: '張三',
+            age: 18,
+            sex: '男'
+        }
+    },
+}
+</script>
+<style>
+.demo {
+    background-color: gray;
+}
+</style>
