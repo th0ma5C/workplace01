a2a929e8f1a3ee8527ebf7f909d06716645beb31
th0ma5C <thomas29111@gmail.com>
2023-12-17 13:30:16 +0800

    1217nodejs練習

--- _practice_nodeJs/1216/fs練習_重新排序.js
+++ _practice_nodeJs/1216/fs練習_重新排序.js
@@ -16,8 +16,8 @@ let fileArr = fs.readdirSync('./新資料夾缺02');
 //     fs.
 // })
 
-console.log(fileArr.length)
-
+// console.log(fileArr.length)
+//#region 
 // fs.mkdir('./新資料夾缺02/重新排序', err => {
 //     if (err) {
 //         console.log('創建資料夾失敗');
@@ -25,7 +25,13 @@ console.log(fileArr.length)
 //     }
 //     console.log('資料夾創建成功');
 // })
+//#endregion
+
 if (!fs.existsSync(`./新資料夾缺02/重新排序`)) {
     fs.mkdirSync(`./新資料夾缺02/重新排序`);
-    console.log('新資料夾創建成功');
+    console.log('重新排序資料夾創建成功');
+}
+
+for (let i = 0; i < (fileArr.length) - 1; i++) {
+    fs.renameSync(`./新資料夾缺02/${fileArr[i]}`, `./新資料夾缺02/重新排序/${i + 1}.js`)
 }
