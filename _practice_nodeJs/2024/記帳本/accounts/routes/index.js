a5dc7f94451294a967d8dadd8f599745c87a2b57
th0ma5C <thomas29111@gmail.com>
2024-03-07 22:25:21 +0800

    0307nodejs

--- _practice_nodeJs/2024/記帳本/accounts/routes/index.js
+++ _practice_nodeJs/2024/記帳本/accounts/routes/index.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* 記帳列表 */
+/**記帳列表 */
 router.get('/account', function (req, res, next) {
-  res.render('list')
+  res.render('list');
 });
-/* 添加列表 */
+/**添加列表 */
 router.get('/account/create', function (req, res, next) {
-  res.send('添加紀錄');
+  res.render('create');
 });
-
+/**添加紀錄 */
+router.post('/account', (req, res) => {
+  // 獲取請求紀錄
+  console.log(req.body);
+  res.send('添加紀錄');
+})
 module.exports = router;
