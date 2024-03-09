929d7da800b5c379b69ee73ccf471284bf90ccf4
th0ma5C <thomas29111@gmail.com>
2024-03-09 19:26:24 +0800

    0309nodejs

--- _practice_nodeJs/2024/記帳本/accounts/routes/index.js
+++ _practice_nodeJs/2024/記帳本/accounts/routes/index.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+// 導入lowdb
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync(__dirname + '/../data/db.json');
+const db = low(adapter);
+
+// 導入shortid
+const shortId = require('shortid');
+
 /**記帳列表 */
 router.get('/account', function (req, res, next) {
   res.render('list');
@@ -11,8 +21,13 @@ router.get('/account/create', function (req, res, next) {
 });
 /**添加紀錄 */
 router.post('/account', (req, res) => {
+  // 生成ID
+  let id = shortId.generate();
   // 獲取請求紀錄
   console.log(req.body);
-  res.send('添加紀錄');
+  // 寫入文件
+  db.get('accounts').unshift({ id: id, ...req.body }).write();
+
+  res.render('success', { msg: '添加成功！', url: '/account' });
 })
 module.exports = router;
