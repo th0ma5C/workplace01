c71f179746fdac7f5192d3d4b661db5a59700d0c
th0ma5C <thomas29111@gmail.com>
2024-03-20 16:50:49 +0800

    0320swiperTest

--- test/vue_testingPlace/src/App.vue
+++ test/vue_testingPlace/src/App.vue
@@ -3,17 +3,17 @@
     <transition-group tag="div" class="img-container"
         :name="transitionName">
         <a href="" v-for="(imgURL, index) in getImgURL"
-            :key="index" @click.prevent
-            v-show="index === show">
+            :key="imgURL" @click.prevent :style="imgSwiper"
+            class="imgSwiper">
             <img :src="imgURL" alt="">
         </a>
     </transition-group>
     <!-- </div> -->
     <div class="btn-container">
         <button class="prev-btn"
-            @click="changeSwiper(show - 1)">&lt;</button>
+            @click="changeSwiper(-1)">&lt;</button>
         <button class="next-btn"
-            @click="changeSwiper(show + 1)">&gt;</button>
+            @click="changeSwiper(1)">&gt;</button>
     </div>
     <div class="pagination-container" style="">
         <span v-for="(img, index) in imgs" :key="img.title"
@@ -22,7 +22,7 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref, watch } from 'vue';
+import { computed, onMounted, ref, watch } from 'vue';
 
 const imgs = [
     { title: 'berry-smoothie' },
@@ -38,33 +38,60 @@ const getImgURL = imgs.map(item => {
     return new URL(`./assets/MainBanner/${item.title}.jpg`, import.meta.url).href;
 });
 
-let show = ref(0);
+let left = ref(0), translateX = ref(0), count = 0
+let imgSwiper = computed(() => ({
+    transform: `translateX(${translateX.value}%)`
+}))
+
 let transitionName = ref('left-in');
 function changeSwiper(n: number) {
-    show.value = n
-}
-function nextSwiper() {
-    show.value++
+
+    // if (n > 0) {
+    //     imgs.push(imgs.shift()!)
+    // } else {
+    //     imgs.unshift(imgs.pop()!)
+    // }
+
+
+    count += n;
+    if (count == imgs.length) {
+        count = 0;
+        translateX.value = 0;
+        return
+    } else if (count < 0) {
+        count = imgs.length - 1;
+        translateX.value = -(imgs.length - 1) * 100;
+        return
+    }
+    translateX.value -= (n * 100);
 }
 function fn(index: number) {
-    for (let i = 0; i <= index; i++) {
-        changeSwiper(i)
+    if (index == count) return;
+    let i = index - count;
+    if (i > 0) {
+        for (let j = 0; j < i; j++) {
+            changeSwiper(1);
+        }
+    } else {
+        for (let j = 0; j > i; j--) {
+            changeSwiper(-1);
+        }
     }
 }
 // let timer = setInterval(nextSwiper, 2000);
-watch(show, (nVal, oVal) => {
-    if (nVal < 0) {
-        show.value = imgs.length - 1
-    } else if (nVal > imgs.length - 1) {
-        show.value = 0
-    } else {
-        if (oVal < 0) transitionName.value = 'left-in'
-        else if (oVal > imgs.length - 1) transitionName.value = 'right-in'
-        else transitionName.value = nVal > oVal ? 'right-in' : 'left-in'
-        console.log(transitionName.value);
-    }
-    console.log(show.value, nVal, oVal);
-})
+// watch(show, (nVal, oVal) => {
+//     if (nVal < 0) {
+//         show.value = imgs.length - 1
+//     } else if (nVal > imgs.length - 1) {
+//         show.value = 0
+//     } else {
+//         if (oVal < 0) transitionName.value = 'left-in'
+//         else if (oVal > imgs.length - 1) transitionName.value = 'right-in'
+//         else transitionName.value = nVal > oVal ? 'right-in' : 'left-in'
+//         console.log(transitionName.value);
+//     }
+//     console.log(show.value, nVal, oVal);
+// })
 
 
 
@@ -78,41 +105,49 @@ onMounted(() => {
 <style lang="scss">
 @import './assets/basic.scss';
 
-.right-in-enter-from {
-    left: 100%
-}
+// .imgSwiper {
+//     transition: all 1s;
+// }
 
-.right-in-enter-active,
-.right-in-leave-active {
-    transition: left 0.5s;
-}
+// .left-in-move {
+//     transition: all 1s;
+// }
 
-.right-in-enter-to,
-.right-in-leave-from {
-    left: 0%
-}
+// .right-in-enter-from {
+//     left: 100%
+// }
 
-.right-in-leave-to {
-    left: -100%
-}
+// .right-in-enter-active,
+// .right-in-leave-active {
+//     transition: left 0.5s;
+// }
 
-.left-in-enter-from {
-    left: -100%
-}
+// .right-in-enter-to,
+// .right-in-leave-from {
+//     left: 0%
+// }
 
-.left-in-enter-active,
-.left-in-leave-active {
-    transition: left 0.5s;
-}
+// .right-in-leave-to {
+//     left: -100%
+// }
 
-.left-in-enter-to,
-.left-in-leave-from {
-    left: 0%
-}
+// .left-in-enter-from {
+//     left: -100%
+// }
 
-.left-in-leave-to {
-    left: 100%
-}
+// .left-in-enter-active,
+// .left-in-leave-active {
+//     transition: left 0.5s;
+// }
+
+// .left-in-enter-to,
+// .left-in-leave-from {
+//     left: 0%
+// }
+
+// .left-in-leave-to {
+//     left: 100%
+// }
 
 body {
     margin: 0;
@@ -126,8 +161,6 @@ body {
     position: relative;
 
     a {
-        // transition: all 0.5s ease-in-out;
-        position: absolute;
 
         img {
             width: 100vw;
