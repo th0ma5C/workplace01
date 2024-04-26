8f8c79353032fb52aff6f870649cb53fcf447814
th0ma5C <thomas29111@gmail.com>
2024-04-26 15:42:11 +0800

    0426test

---
+++ test/vue_testingPlace/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+.container {
+    height: 100vh;
+    width: 100vw;
+    background: lightblue;
+    mask: url('./assets/資產 1-100.jpg');
+}
+</style>
