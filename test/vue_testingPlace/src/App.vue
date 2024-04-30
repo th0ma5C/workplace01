2ff2de351f34a7d9b8fa5e3b6a4fa8ad3312898a
th0ma5C <thomas29111@gmail.com>
2024-04-30 21:28:06 +0800

    0430vueTest

--- test/vue_testingPlace/src/App.vue
+++ test/vue_testingPlace/src/App.vue
@@ -1,24 +1,33 @@
 <template>
-    <div class="container">
-        <div></div>
+    <div :style="test">
+        abcde
+    </div>
+    <div v-if="i" @click="change">
+        123456
+    </div>
+    <div v-else @click="change">
+        7891011
     </div>
 </template>
 
 <script setup lang="ts">
+import { computed, ref } from 'vue';
+let i = ref(true);
+let j = ref(1)
+function change() {
+    i.value = !i.value
+    j.value++;
+}
+let test = computed(() => ({
+    'font-size': `${j.value}rem`
+}))
 
 </script>
 
 <style scoped lang="scss">
-.container {
-    height: 100vh;
-    width: 100vw;
-    mask-image: url('./assets/1.png');
-    mask-repeat: no-repeat;
+$size: 1rem;
 
-    div {
-        background: lightblue;
-        width: 100%;
-        height: 100%;
-    }
+div {
+    font-size: var(--num, 1rem);
 }
 </style>
