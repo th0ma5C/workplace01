db1c5c3e92c4566b84aef17a25b9edf7de8020d4
th0ma5C <thomas29111@gmail.com>
2024-06-18 20:49:14 +0800

    0618test

--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const filePath = path.resolve(__dirname, 'test.json');
+const nFilePath = path.resolve(__dirname, 'nTest.json');
 
 // 读取JSON文件
 fs.readFile(filePath, 'utf8', (err, data) => {
@@ -14,13 +15,24 @@ fs.readFile(filePath, 'utf8', (err, data) => {
     let jsonData = JSON.parse(data);
 
     //修改数据
-    jsonData.forEach(el => {
-        el.date = new Date(el.date);
-    });
-    const updatedData = JSON.stringify(jsonData, null, 2);
+    let nData = jsonData.reduce((acc, item) => {
+        if (!acc[item.label]) {
+            acc[item.label] = [];
+        }
+        // Push the item into the correct category array
+        acc[item.label].push({
+            date: item.date,
+            title: item.title,
+            content: item.content
+        });
+        return acc;
+    }, {})
+
+
+    const updatedData = JSON.stringify(nData, null, 2);
 
     // 将修改后的JSON写回文件
-    fs.writeFile(filePath, updatedData, 'utf8', (err) => {
+    fs.writeFile(nFilePath, updatedData, 'utf8', (err) => {
         if (err) {
             console.error('Error writing file:', err);
             return;
