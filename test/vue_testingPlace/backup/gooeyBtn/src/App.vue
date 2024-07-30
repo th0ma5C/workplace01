d331329f85390baca4b7408c888440ce9e8ee6e0
th0ma5C <thomas29111@gmail.com>
2024-07-30 22:01:56 +0800

    0730 test

---
+++ test/vue_testingPlace/backup/gooeyBtn/src/App.vue
@@ -0,0 +1,217 @@
+<template>
+    <div class="container">
+        <div class="btnwrapper">
+            <button class="left">左左左左左</button>
+            <button class="right">右右右右</button>
+            <div class="blocker1"></div>
+            <div class="blocker2"></div>
+        </div>
+        <div class="btnwrapper2">
+            <button class="left">左左左左左</button>
+            <button class="right">右右右右</button>
+            <div class="bag">
+                <svg width="176" height="51"
+                    viewBox="0 0 176 51" fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <g id="btnBac" filter="url('#btn')">
+                        <rect id="center" class="center"
+                            y="13" width="72" height="26"
+                            fill="currentColor" />
+                        <path id="right" class="right"
+                            d="M176 51L104.133 51L88 8.40846e-06L176 7.15256e-07L176 51Z"
+                            fill="currentColor" />
+                        <path id="left" class="left"
+                            d="M0 0H71.8667L88 51H0V0Z"
+                            fill="currentColor" />
+                    </g>
+
+                    <filter id="btn">
+                        <feGaussianBlur in="SourceGraphic"
+                            result="blur"
+                            stdDeviation="5" />
+                        <feColorMatrix in="blur"
+                            mode="matrix"
+                            values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7"
+                            result="btn" />
+                    </filter>
+                </svg>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+.container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 2rem;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
+}
+
+.btnwrapper {
+    display: flex;
+    // gap: 1rem;
+    position: relative;
+    border: 1px solid black;
+    border-radius: 25px;
+    overflow: hidden;
+
+    button {
+        cursor: pointer;
+        border: none;
+        width: 100px;
+        height: 50px;
+        background-color: transparent;
+        font-size: 16px;
+
+        transition: background-color 0.5s ease;
+
+        &:hover {
+            // background-color: aqua
+        }
+    }
+
+    &::before {
+        border-radius: 25px 0 0 25px;
+        content: '';
+        // display: flex;
+        // justify-content: center;
+        // align-items: center;
+        // text-align: center;
+        width: 100px;
+        height: 50px;
+        position: absolute;
+        left: 0;
+        z-index: -1;
+        background-color: aqua;
+        transition: transform 0.5s ease, border-radius 0.5s ease;
+    }
+
+    @keyframes hide {
+        from {
+            background-color: transparent;
+        }
+
+        to {
+            background-color: white;
+        }
+    }
+
+    &:has(.right:hover) {
+
+        &::before {
+            border-radius: 0 25px 25px 0;
+            transform: translateX(calc(100px));
+        }
+
+        .blocker1 {
+            z-index: -2;
+            transform: translateX(calc(-50% + 100px)) skew(15deg);
+        }
+
+        .blocker2 {
+            // width: 15px;
+            z-index: -1;
+            transform: translateX(calc(-50% + 100px)) skew(15deg);
+        }
+    }
+
+    .blocker1 {
+        position: absolute;
+        left: 50%;
+        top: 0;
+        z-index: -1;
+        width: 15px;
+        height: 50px;
+        transform: translateX(-50%) skew(15deg);
+        background-color: white;
+        // border: 1px solid black;
+        transition: transform 0.5s ease;
+    }
+
+    .blocker2 {
+        position: absolute;
+        left: 0px;
+        top: 0;
+        z-index: -2;
+        width: 15px;
+        height: 50px;
+        transform: translateX(-50%) skew(15deg);
+        background-color: white;
+        // border: 1px solid black;
+        transition: transform 0.5s ease;
+    }
+}
+
+.btnwrapper2 {
+    display: flex;
+    position: relative;
+    border: 1px solid black;
+    border-radius: 25px;
+    overflow: hidden;
+
+    button {
+        cursor: pointer;
+        border: none;
+        width: 100px;
+        height: 50px;
+        background-color: transparent;
+        font-size: 16px;
+        transition: background-color 0.5s ease;
+    }
+
+    &:has(.right:hover) {
+        svg {
+            .left {
+                scale: 0;
+            }
+
+            .center {
+                transform: translateX(100px);
+            }
+
+            .right {
+                scale: 1;
+            }
+        }
+    }
+
+    .bag {
+        display: flex;
+        position: absolute;
+        z-index: -1;
+
+        svg {
+            width: 200px;
+            height: 80px;
+            transform: translateY(-20%);
+
+            .left {
+                transition: all 0.5s ease;
+                transform-origin: center;
+                fill: aqua;
+            }
+
+            .center {
+                transition: all 0.5s ease;
+                transform-origin: center;
+                fill: aqua;
+            }
+
+            .right {
+                scale: 0;
+                transition: all 0.5s ease;
+                transform-origin: center;
+                fill: aqua;
+            }
+        }
+    }
+}
+</style>
--- test/vue_testingPlace/src/App.vue
+++ test/vue_testingPlace/src/App.vue
@@ -1,217 +1,89 @@
 <template>
-    <div class="container">
-        <div class="btnwrapper">
-            <button class="left">左左左左左</button>
-            <button class="right">右右右右</button>
-            <div class="blocker1"></div>
-            <div class="blocker2"></div>
-        </div>
-        <div class="btnwrapper2">
-            <button class="left">左左左左左</button>
-            <button class="right">右右右右</button>
-            <div class="bag">
-                <svg width="176" height="51"
-                    viewBox="0 0 176 51" fill="none"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <g id="btnBac" filter="url('#btn')">
-                        <rect id="center" class="center"
-                            y="13" width="72" height="26"
-                            fill="currentColor" />
-                        <path id="right" class="right"
-                            d="M176 51L104.133 51L88 8.40846e-06L176 7.15256e-07L176 51Z"
-                            fill="currentColor" />
-                        <path id="left" class="left"
-                            d="M0 0H71.8667L88 51H0V0Z"
-                            fill="currentColor" />
-                    </g>
-
-                    <filter id="btn">
-                        <feGaussianBlur in="SourceGraphic"
-                            result="blur"
-                            stdDeviation="5" />
-                        <feColorMatrix in="blur"
-                            mode="matrix"
-                            values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7"
-                            result="btn" />
-                    </filter>
-                </svg>
+    <div class="expandable-grid-container">
+        <div class="grid" :style="gridStyle">
+            <div v-for="i in 12" :key="i" class="grid-item">
+                Item {{ i }}
             </div>
         </div>
+        <button @click="toggleExpand" class="expand-button">
+            {{ isExpanded ? '收起' : '展開' }}
+        </button>
     </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
+import { defineComponent, ref, computed } from 'vue'
 
+export default defineComponent({
+    name: 'ExpandableGrid',
+    setup() {
+        const isExpanded = ref(false)
+        const gridHeight = ref('200px')  // 初始高度，可以根據需要調整
+
+        const toggleExpand = () => {
+            isExpanded.value = !isExpanded.value
+            gridHeight.value = isExpanded.value ? '300px' : '200px'  // 展開後的高度，可以根據需要調整
+        }
+
+        const gridStyle = computed(() => ({
+            height: gridHeight.value,
+            gridTemplateRows: isExpanded.value ? 'repeat(3, 1fr)' : 'repeat(2, 1fr)'
+        }))
+
+        return {
+            isExpanded,
+            toggleExpand,
+            gridStyle
+        }
+    }
+})
 </script>
 
-<style scoped lang="scss">
-.container {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    gap: 2rem;
-    align-items: center;
-    width: 100vw;
-    height: 100vh;
+<style scoped>
+.expandable-grid-container {
+    width: 100%;
+    max-width: 800px;
+    margin: 0 auto;
 }
 
-.btnwrapper {
-    display: flex;
-    // gap: 1rem;
-    position: relative;
-    border: 1px solid black;
-    border-radius: 25px;
+.grid {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 10px;
+    transition: all 0.5s ease-in-out;
     overflow: hidden;
-
-    button {
-        cursor: pointer;
-        border: none;
-        width: 100px;
-        height: 50px;
-        background-color: transparent;
-        font-size: 16px;
-
-        transition: background-color 0.5s ease;
-
-        &:hover {
-            // background-color: aqua
-        }
-    }
-
-    &::before {
-        border-radius: 25px 0 0 25px;
-        content: '';
-        // display: flex;
-        // justify-content: center;
-        // align-items: center;
-        // text-align: center;
-        width: 100px;
-        height: 50px;
-        position: absolute;
-        left: 0;
-        z-index: -1;
-        background-color: aqua;
-        transition: transform 0.5s ease, border-radius 0.5s ease;
-    }
-
-    @keyframes hide {
-        from {
-            background-color: transparent;
-        }
-
-        to {
-            background-color: white;
-        }
-    }
-
-    &:has(.right:hover) {
-
-        &::before {
-            border-radius: 0 25px 25px 0;
-            transform: translateX(calc(100px));
-        }
-
-        .blocker1 {
-            z-index: -2;
-            transform: translateX(calc(-50% + 100px)) skew(15deg);
-        }
-
-        .blocker2 {
-            // width: 15px;
-            z-index: -1;
-            transform: translateX(calc(-50% + 100px)) skew(15deg);
-        }
-    }
-
-    .blocker1 {
-        position: absolute;
-        left: 50%;
-        top: 0;
-        z-index: -1;
-        width: 15px;
-        height: 50px;
-        transform: translateX(-50%) skew(15deg);
-        background-color: white;
-        // border: 1px solid black;
-        transition: transform 0.5s ease;
-    }
-
-    .blocker2 {
-        position: absolute;
-        left: 0px;
-        top: 0;
-        z-index: -2;
-        width: 15px;
-        height: 50px;
-        transform: translateX(-50%) skew(15deg);
-        background-color: white;
-        // border: 1px solid black;
-        transition: transform 0.5s ease;
-    }
 }
 
-.btnwrapper2 {
-    display: flex;
-    position: relative;
-    border: 1px solid black;
-    border-radius: 25px;
-    overflow: hidden;
+.grid-item {
+    background-color: #f0f0f0;
+    padding: 20px;
+    text-align: center;
+    border-radius: 5px;
+}
 
-    button {
-        cursor: pointer;
-        border: none;
-        width: 100px;
-        height: 50px;
-        background-color: transparent;
-        font-size: 16px;
-        transition: background-color 0.5s ease;
-    }
+.grid-item:nth-child(n+9) {
+    opacity: 0;
+    transform: translateY(-20px);
+    transition: opacity 0.5s ease-in-out, transform 0.5s ease-in-out;
+}
 
-    &:has(.right:hover) {
-        svg {
-            .left {
-                scale: 0;
-            }
+.grid[style*="300px"] .grid-item:nth-child(n+9) {
+    opacity: 1;
+    transform: translateY(0);
+}
 
-            .center {
-                transform: translateX(100px);
-            }
+.expand-button {
+    margin-top: 20px;
+    padding: 10px 20px;
+    background-color: #4CAF50;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background-color 0.3s;
+}
 
-            .right {
-                scale: 1;
-            }
-        }
-    }
-
-    .bag {
-        display: flex;
-        position: absolute;
-        z-index: -1;
-
-        svg {
-            width: 200px;
-            height: 80px;
-            transform: translateY(-20%);
-
-            .left {
-                transition: all 0.5s ease;
-                transform-origin: center;
-                fill: aqua;
-            }
-
-            .center {
-                transition: all 0.5s ease;
-                transform-origin: center;
-                fill: aqua;
-            }
-
-            .right {
-                scale: 0;
-                transition: all 0.5s ease;
-                transform-origin: center;
-                fill: aqua;
-            }
-        }
-    }
+.expand-button:hover {
+    background-color: #45a049;
 }
 </style>
