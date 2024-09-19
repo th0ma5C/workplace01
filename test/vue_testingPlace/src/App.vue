47a53cf754e39aade2de94b004448a1467483312
th0ma5C <thomas29111@gmail.com>
2024-09-19 22:42:40 +0800

    0919 update packageJSON

--- test/vue_testingPlace/src/App.vue
+++ test/vue_testingPlace/src/App.vue
@@ -1,209 +1,83 @@
 <template>
-    <div class="header">
-        <div class="filter">
-            <h2>Selected name: {{ filterWord }}</h2>
-            <div v-for="(item, index) in data" :key="index">
-                <input type="checkbox" v-model="filterWord"
-                    :id="item.name" :value="item.name">
-                <label :for="item.name">{{ item.name
-                    }}</label>
-            </div>
-        </div>
-
-        <div class="sort">
-            <h2>Selected: {{ sortOrder }}</h2>
-            <select v-model="sortOrder">
-                <option disabled value="">Please select one
-                </option>
-                <option v-for="(item, index) in sortList"
-                    :key="index">{{ item }}</option>
-            </select>
-        </div>
-
-        <div class="btnGroup">
-            <button @click="plus">新增</button>
-            <button @click="minus">刪除</button>
-            <button @click="shuffle">洗牌</button>
-            <button @click="reset">重置</button>
-        </div>
+    <div>
+        {{ state.count }}
     </div>
-
-    <div class="container" ref="container">
-        <div v-for="(item, index) in showData" :key="index"
-            ref="box" class="box">
-            {{ item.name }} <br>
-            {{ item.id }}
-        </div>
-    </div>
+    <button @click="state.count = 2;">+++</button>
 </template>
 
 <script setup lang="ts">
-import { computed, ref, type Ref, watch, nextTick } from 'vue';
-import { shuffle as _shuffle } from 'lodash-es';
-import { gsap } from 'gsap'
+// import { computed } from 'vue';
 
-let data = ref([
-    {
-        name: "tom",
-        id: 123
-    },
-    {
-        name: "jack",
-        id: 456
-    },
-    {
-        name: "ken",
-        id: 789
-    },
-    {
-        name: "dick",
-        id: 1011
-    },
-    {
-        name: "musk",
-        id: 1213
-    },
-])
+class Dep {
+    constructor() {
+        this.subscribers = new Set();
+    }
 
-let filterWord: Ref<string[]> = ref([]);
-let sortOrder = ref('asc');
-let sortList = ref(['asc', 'desc'])
-
-type CompareFn<T> = (a: T, b: T) => number;
-
-const compareFn = <T>(key: (item: T) => any, order: number): CompareFn<T> => {
-    let isDESC = order !== 0;
-    return (a: T, b: T): number => {
-        const aValue = key(a);
-        const bValue = key(b);
-
-        if (aValue < bValue) {
-            return isDESC ? -1 : 1;
-        } else if (aValue > bValue) {
-            return isDESC ? 1 : -1;
-        } else {
-            return 0;
+    depend() {
+        if (activeUpdate) {
+            this.subscribers.add(activeUpdate);
         }
     }
+
+    notify() {
+        this.subscribers.forEach(sub => sub());
+    }
 }
 
-let showData = computed(() => {
-    let result = data.value;
+let activeUpdate = null;
+function watchEffect(update) {
+    activeUpdate = update;
+    update();  // 執行一次函數來觸發依賴收集
+    activeUpdate = null;
+}
 
-    if (filterWord.value.length != 0) {
-        result = result.filter((item) => {
-            for (let i of filterWord.value) {
-                if (item.name == i) return true
-            }
-        })
-    }
+// 建立一個響應式的代理
+function reactive(target) {
+    const dep = new Dep();
 
-
-    switch (sortOrder.value) {
-        case 'asc':
-            result.sort(compareFn(item => item.id, 1))
-            break;
-        case 'desc':
-            result.sort(compareFn(item => item.id, 0))
-            break;
-        default:
-            result.sort(compareFn(item => item.id, 1))
-            break;
-    }
-
-    return result
-})
-
-watch(showData, () => {
-    nextTick(() => {
-        const elements = document.querySelectorAll('.box');
-        gsap.from(elements, {
-            opacity: 0,
-            y: 20,
-            duration: 0.5,
-            stagger: 0.1
-        });
+    return new Proxy(target, {
+        get(obj, key) {
+            dep.depend();
+            return obj[key];
+        },
+        set(obj, key, value) {
+            obj[key] = value;
+            dep.notify();
+            return true;
+        }
     });
-});
-
-function plus() {
-    data.value.push({
-        name: "tom",
-        id: 123
-    })
 }
 
-function minus() {
-    data.value.pop()
+// 計算屬性的類似實現
+function computed(getter) {
+    let value;
+    let dirty = true;
+
+    const effect = () => {
+        if (dirty) {
+            value = getter();
+            dirty = false;
+        }
+        return value;
+    };
+
+    return {
+        get value() {
+            watchEffect(effect);
+            return effect();
+        }
+    };
 }
 
-function shuffle() {
-    data.value = _shuffle(data.value);
-    console.log(data.value);
-}
+// 示範使用
+const state = reactive({ count: 1 });
 
-function reset() {
-    data.value = [
-        {
-            name: "tom",
-            id: 123
-        },
-        {
-            name: "jack",
-            id: 456
-        },
-        {
-            name: "ken",
-            id: 789
-        },
-        {
-            name: "dick",
-            id: 1011
-        },
-        {
-            name: "musk",
-            id: 1213
-        },
-    ]
-}
+const doubled = computed(() => state.count * 2);
+
+console.log(doubled.value); // 2
+state.count = 2;
+console.log(doubled.value); // 4
+
 </script>
 
-<style scoped lang="scss">
-.header {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 2rem;
-
-    .filter {
-        width: 600px;
-    }
-
-    .sort {
-        align-self: self-start;
-        width: 150px;
-    }
-}
-
-.container {
-    // width: 100vw;
-    // height: 100vh;
-    display: grid;
-    justify-items: center;
-    grid-template-columns: repeat(2, 1fr);
-    gap: 3rem;
-    margin-top: 2rem;
-    padding: 3rem;
-    outline: 1px solid black;
-
-    .box {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        width: 100px;
-        height: 100px;
-        outline: 1px solid black;
-        background-color: antiquewhite;
-    }
-}
-</style>
+<style scoped></style>
