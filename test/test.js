e823e359e5aa0d7932464ef5b4feff0306e29750
th0ma5C <thomas29111@gmail.com>
2024-09-25 19:57:44 +0800

    0925 test

--- test/test.js
+++ test/test.js
@@ -4,50 +4,18 @@ const path = require('path');
 const filePath = path.resolve(__dirname, 'test.json');
 const nFilePath = path.resolve(__dirname, 'nTest.json');
 
-function getRandomDateInPastYear() {
-    const currentDate = new Date();
-    const currentYear = currentDate.getFullYear();
-    const currentMonth = currentDate.getMonth();
-    const currentDay = currentDate.getDate();
 
-    // 生成一个从去年同一天开始到今天的随机时间戳
-    const start = new Date(currentYear - 1, currentMonth, currentDay);
-    const end = currentDate;
-    const randomTimeStamp = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
-
-    // 获取随机日期的年、月、日并格式化
-    const year = randomTimeStamp.getFullYear();
-    const month = String(randomTimeStamp.getMonth() + 1).padStart(2, '0');
-    const day = String(randomTimeStamp.getDate()).padStart(2, '0');
-
-    return `${year}/${month}/${day}`;
-}
-
-function getRandomInteger(min, max) {
-    // 确保min和max是整数
-    min = Math.ceil(min);
-    max = Math.floor(max);
-
-    // 生成min（包含）到max（包含）之间的随机整数
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
 
 fs.readFile(filePath, 'utf8', (err, rawData) => {
     if (err) console.log(err);
 
     let data = JSON.parse(rawData);
-    // for (let i of data) {
-    //     i.items.forEach((el) => {
-    //         el.rating = Number(getRandomInteger(2, 5));
-    //         el.date = getRandomDateInPastYear();
-    //     })
-    //     console.log(i);
-    // }
 
-    data.forEach((item) => {
-        item.date = getRandomDateInPastYear();
-        item.rating = Number(getRandomInteger(2, 5));
+    data.forEach((cate) => {
+        cate.items.forEach((item) => {
+            item.tags.splice(4, 2)
+        })
     })
 
     fs.writeFile(nFilePath, JSON.stringify(data, null, 2), err => {
--- test/vue_testingPlace/src/App.vue
+++ test/vue_testingPlace/src/App.vue
@@ -1,82 +1,14 @@
 <template>
     <div>
-        {{ state.count }}
+        11111111111
+        <span v-show="foo">22222222222</span>
     </div>
-    <button @click="state.count = 2;">+++</button>
 </template>
 
 <script setup lang="ts">
-// import { computed } from 'vue';
+import { ref } from 'vue'
 
-class Dep {
-    constructor() {
-        this.subscribers = new Set();
-    }
-
-    depend() {
-        if (activeUpdate) {
-            this.subscribers.add(activeUpdate);
-        }
-    }
-
-    notify() {
-        this.subscribers.forEach(sub => sub());
-    }
-}
-
-let activeUpdate = null;
-function watchEffect(update) {
-    activeUpdate = update;
-    update();  // 執行一次函數來觸發依賴收集
-    activeUpdate = null;
-}
-
-// 建立一個響應式的代理
-function reactive(target) {
-    const dep = new Dep();
-
-    return new Proxy(target, {
-        get(obj, key) {
-            dep.depend();
-            return obj[key];
-        },
-        set(obj, key, value) {
-            obj[key] = value;
-            dep.notify();
-            return true;
-        }
-    });
-}
-
-// 計算屬性的類似實現
-function computed(getter) {
-    let value;
-    let dirty = true;
-
-    const effect = () => {
-        if (dirty) {
-            value = getter();
-            dirty = false;
-        }
-        return value;
-    };
-
-    return {
-        get value() {
-            watchEffect(effect);
-            return effect();
-        }
-    };
-}
-
-// 示範使用
-const state = reactive({ count: 1 });
-
-const doubled = computed(() => state.count * 2);
-
-console.log(doubled.value); // 2
-state.count = 2;
-console.log(doubled.value); // 4
+const foo = ref(false)
 
 </script>
 
